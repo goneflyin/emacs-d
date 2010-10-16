f56d2267fdc55d231a2001ab8f3a19f61225b641
Scott Pfister <spfister@suisei.local>
2010-10-16 12:20:22 -0500

    add files forgotten in previous commits

---
+++ customizations/modes.el
@@ -0,0 +1,12 @@
+;; allow us to look at whitespace
+(require 'whitespace)
+(require 'show-wspace)
+;;(add-hook 'font-lock-mode-hook 'show-ws-highlight-tabs)
+
+
+;; scala-mode
+(require 'scala-mode-auto)
+;; (require 'font-lock) ;; why did stevej have this here??
+;; (require 'ensime)
+
+(require 'org-mode)
