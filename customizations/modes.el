841edbded7cef3108363d5aa6d61b2812a881396
Scott Pfister <spfister@suisei.local>
2010-11-29 15:19:42 -0600

    add more configuration for org-mode

--- customizations/modes.el
+++ customizations/modes.el
@@ -10,7 +10,15 @@
 (require 'ensime)
 
 ;; org-mode
-;; (require 'org-mode)
+;; (require 'org-mode) ;; handled by (vendor 'org) in init.el
+(add-to-list 'load-path "~/.emacs.d/vendor/org")
+(require 'org-install)
+(setq org-directory "~/.org")
+(setq org-mobile-directory "~/Dropbox/MobileOrg")
+(setq org-default-notes-file (concat org-directory "/notes.org"))
+(setq org-startup-indented t)
+(define-key global-map "\C-cc" 'org-capture)
+(global-set-key "\C-ca" 'org-agenda)
 
 ;; groovy-mode
 (require 'groovy-mode)
