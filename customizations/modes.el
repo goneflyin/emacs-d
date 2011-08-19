ee70d8cb8515c1c84372902324482f1f6e53cd50
Scott Pfister <scott.pfister@gmail.com>
2011-08-19 07:12:21 -0500

    add coffee mode

--- customizations/modes.el
+++ customizations/modes.el
@@ -46,3 +46,11 @@
 ;; groovy-mode
 (require 'groovy-mode)
 (add-to-list 'auto-mode-alist '("\\.gradle$" . groovy-mode))
+
+;; coffee-mode
+(defun coffee-custom ()
+  "coffee-mode-hook"
+  (set (make-local-variable 'tab-width) 2))
+
+(add-hook 'coffee-mode-hook
+	  '(lambda() (coffee-custom)))
