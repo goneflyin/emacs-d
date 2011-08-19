8670782b09058b5b60081433222bdfc4ff3f4bc5
Scott Pfister <scott.pfister@gmail.com>
2011-08-19 07:15:22 -0500

    try setting Menlo-9 for a default font, see how it feels

--- customizations-mac/ui.el
+++ customizations-mac/ui.el
@@ -2,4 +2,7 @@
 ;; TODO: Setting this font hoses the startup screen in Emacs 23.2 (but not 23.1)
 ;;       Is there another, more appropriate way, to set the default font for 
 ;;       the whole app?
-(set-default-font "-apple-DejaVu_Sans_Mono-medium-normal-normal-*-10-*-*-*-m-0-iso10646-")
+;;(set-default-font "-apple-Menlo-medium-normal-normal-*-10-*-*-*-m-0-iso10646-1")
+;;(set-default-font "-apple-DejaVu_Sans_Mono-medium-normal-normal-*-10-*-*-*-m-0-iso10646-")
+
+(set-face-attribute 'default nil :font "Menlo-9")
